82e802706fae468abc7c787136cfe8fc13a15e92
Yigit Yalcin <yigityalciin@hotmail.com>
2024-07-10 19:05:28 +0300

    scroll down button

--- TaluIbrahimTaner/Controllers/HomeController.cs
+++ TaluIbrahimTaner/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace TaluIbrahimTaner.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Anasayfa()
         {
             return View();
         }
